ac6c0c4c33eb5006981c318adb76e3a32e3f84c9
Vitaly-V <gtopbox@gmail.com>
2019-05-24 15:26:39 +0300

    Add animation loop

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -28,10 +28,24 @@ class _WelcomeScreenState extends State<WelcomeScreen>
 
     controller.forward();
 
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        controller.reverse(from: 1.0);
+      } else if (status == AnimationStatus.dismissed) {
+        controller.forward();
+      }
+    });
+
     controller.addListener(() {
       setState(() {});
       print(animation.value);
     });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
